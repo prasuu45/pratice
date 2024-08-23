356df69363ba35f7c478f646ea518eacc8eca1a4
prasuu45 <prasuuchow93@gmail.com>
2024-08-23 14:47:12 +0530

    variable2

---
+++ variable2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "please entre your username ::"
+read  -s USERNAME  
+echo "username entered is : $USERNAME"
+
+echo "please entre your password ::"
+read  -s PASSWORD
+echo "password entered is : $USERNAME"
