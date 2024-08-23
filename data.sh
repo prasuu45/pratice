2fb6f333ff4672f5e3634b4f86b8dfd0c831ce36
prasuu45 <prasuuchow93@gmail.com>
2024-08-23 14:34:01 +0530

    data

---
+++ data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# conversation b/w two friends 
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 : hello $PERSON2 what asudden surprise"
+echo "$PERSON2 : sudden surprise me to "
+echo "$PERSON1 : how are you"
+echo "$PERSON2 : i am fine. how about you"
+echo "$PERSON1 : i am doing good. what's going on"
+echo "$PERSON2: i stared learing shell scripting"
