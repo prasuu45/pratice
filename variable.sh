a63a8b4c01754903b3e413d98491a43863a2176e
prasuu45 <prasuuchow93@gmail.com>
2024-08-23 14:31:34 +0530

    variables

---
+++ variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# conversation b/w two friends 
+PERSON1=RAMESH
+PERSON2=RAJESH
+
+echo "$PERSON1 : hello $PERSON2 what asudden surprise"
+echo "$PERSON2 : sudden surprise me to "
+echo "$PERSON1 : how are you"
+echo "$PERSON2 : i am fine. how about you"
+echo "$PERSON1 : i am doing good. what's going on"
+echo "$PERSON2: i stared learing shell scripting"
