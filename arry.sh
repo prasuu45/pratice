f127e180beb6eb0995808f4a58f38e0f2becb23d
prasuu45 <prasuuchow93@gmail.com>
2024-08-23 15:01:30 +0530

    arry

---
+++ arry.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# index starts from 0 size is 3
+FRUITS=( "APPLE" "KIVI" "ORANGE")
+
+echo "frist friut is : ${FRUITS[o]}"
+echo "second friut is : ${FRUITS[1]}"
+echo "third friut is : ${FRUITS[2]}"
+
+
+
+echo "frist friut is : ${FRUITS[@]}"
+
