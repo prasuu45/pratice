98c8f455fce14f5d1270957ad3ad56a300299111
prasuu45 <prasuuchow93@gmail.com>
2024-08-23 14:02:39 +0530

    conversation

---
+++ conversation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# conversation b/w two friends 
+
+echo "ramesh : hello suresh what asudden surprise"
+echo "suresh : sudden surprise me to "
+echo "ramesh : how are you"
+echo "suresh : i am fine. how about you"
+echo "ramesh : i am doing good. what's going on"
+echo "suresh : i stared learing shell scripting"
